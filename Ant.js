b35865b95b0657a3f9fec8c090ceca651cfa8ab8
A.R.Rakha <NEXT16279@edu.nextkbh.dk>
2026-02-20 16:36:06 +0100

    Exploring Ant

--- Ant.js
+++ Ant.js
@@ -1,7 +1,55 @@
 class Ant{
-  constructor(){
+  constructor(x,y,angle=0){
+    this.pos=createVector(x,y);
+    this.vel=createVector(cos(angle),sin(angle));
+    this.acc=createVector(0,0);
 
+    this.angle=angle;
+
+    this.desiredDir=createVector(0,0);
+    this.desiredVel=createVector(0,0);
+    this.desiredTurningForce=createVector(0,0);
+    
+    this.maxSpeed=1;
+    this.turningStrength=1;
+    this.wanderStrength=0.1;
   }
 
-  
+  show(scale){
+    noStroke();
+    fill(0)
+    translate(this.pos.x,this.pos.y)
+    rotate(this.angle);
+    ellipse(0, 0, 4*scale,1*scale);
+    ellipse(0+3*scale,0, 2.5*scale,2*scale);
+    ellipse(0-4*scale,0, 4*scale,2*scale);
+    rotate(-this.angle);
+    translate(-this.pos.x,-this.pos.y)
+    
+  }
+
+  move(){
+    let randomUnitVector= p5.Vector.random2D();
+    randomUnitVector.mult(this.wanderStrength)
+    this.desiredDir.add(randomUnitVector)
+    this.desiredDir.normalize();
+
+    this.desiredVel=this.desiredDir.copy();
+    this.desiredVel.mult(this.maxSpeed);
+    
+    this.desiredTurningForce=p5.Vector.sub(this.desiredVel, this.vel);
+    this.desiredTurningForce.mult(this.turningStrength);
+    this.acc=this.desiredTurningForce.copy();
+    this.acc.limit(this.turningStrength);
+        
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+
+    this.angle=atan2(this.vel.y,this.vel.x);
+
+    }
+  update(){
+    this.move()
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
 let canvasSize=[1500, 700];
 
+let total=1;
+let ants=[];
+
 function setup() 
 {
 	createCanvas(canvasSize[0],canvasSize[1]);
 
+	for (let i = 0; i < total; i++) {
+		ants.push(new Ant(30,30,random(0,PI)))
+	}
 }
 
 function draw()
 {
 	background(220);
+	for (let i = 0; i < total; i++) {
+		ants[i].update();
+		ants[i].show(1);
+	}
+
 
 }
